3ff2048920952463f94149606e0425af322a7091
mopah emma <mopahemma@gmail.com>
2020-06-23 20:55:19 +0100

    create model and add assets

---
+++ Landmarks/LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Mopilo on 23/06/2020.
+//  Copyright © 2020 Mopilo. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarkData[0])
+    }
+}
